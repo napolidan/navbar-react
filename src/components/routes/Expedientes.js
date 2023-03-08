f056e1f9cca2c7909393f18c78828047de1fb396
napolidan <danielnapnapolidan@gmail.com>
2023-03-08 13:46:19 -0500

    fixed details and started expedientes section

--- src/components/routes/Expedientes.js
+++ src/components/routes/Expedientes.js
@@ -2,8 +2,23 @@ import React from "react";
 
 const Expedientes = () => {
     return(
-        <div className="content">
+        <div className="content expedientes">
 
+            <div className="title-holder">
+
+                <h4 className="title">Expedientes</h4>
+
+            </div>
+
+            <div className="white-container">
+
+                <h5>Datos personales</h5>
+
+                <form>
+                    
+                </form>
+
+            </div>
 
         </div>
     )
